027d98151ec3102140dc06265577cea7b0677ee9
Bry Cox <carl.cc@gmail.com>
2019-08-11 17:21:16 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 150 }
-  validates :summary, length: { maximum: 300 }
+  validates :summary, length: { maximum: 150 }
 end
